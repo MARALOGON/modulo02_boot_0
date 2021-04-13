08f1e71d4b97cfedc4dba1b8cef0f8ba57ec0d55
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-04-13 12:31:09 +0200

    Ejemplo turtle. Introducción a objetos

---
+++ objetos.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+tortuguita = turtle.Turtle() #Los objetos van en mayusculas, la minuscula es el modulo
+
+otraTortuguita = turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otraTortuguita.color('green')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
