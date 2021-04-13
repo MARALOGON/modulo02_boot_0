eeff61f9785423b2d54c5096670bcc26316a9d87
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-04-13 12:30:11 +0200

    Operadores Map,Filter,Reduce

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -19,7 +19,24 @@ sumatorio = reduce(lambda x, y: x + y, lista)
 
 suma100 = reduce(lambda x, y: x + y, range(101))
 
-sumatorioDobles = reduce(lambda x, y: x + y*2, lista)
+
+
+#Reduce tiene el problema de que no cuenta el primer elemento para realizar la operacion que si aplica al resto
+#Por eso, el resultado de esta funcion lambda es 53 y no 54 como deberia ser
+#para subsanar ese error hay que:
+#1. crear una copia de la lista
+
+l=lista[:] #Con esta sintaxis de [:] se crea una copia de todo lo que contiene la lista
+#Esto sirve para clonar la lista, por si acaso volvemos a n ecesitar la original y queremos modificar la copia,,
+#que la original se mantenga. Si solo asignaramos l=lista, los cambios que hicieramos en l
+#tambien se harian en lista
+
+
+#2. Se añade el elmento neutro para que la funcion empiece a operar en la posicion cero de la lista
+l.insert(0,0)
+
+sumatorioDobles = reduce(lambda x, y: x + y*2, l)
+
 
 print(list(listaPares))
 print(list(listaPares1))
