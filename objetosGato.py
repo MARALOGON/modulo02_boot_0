181a01961d2c433279f4642fc901490af4eb3d3b
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-04-14 12:26:07 +0200

    Primera version de objeto gato

---
+++ objetosGato.py
@@ -0,0 +1,6 @@
+class Gato():
+    def __init__(self, n, e, p): #self es una clase vacia que se genera para que contenga los atributos siguientes (nombre, edad, peso)
+        self.nombre = n
+        self.edad = e
+        self.peso = p
+        
